e3a673d838064e8f2257fbbe3eba7000e6263133
gustavpirich <gustav@pirich.at>
2024-10-31 14:51:03 +0100

    some very important chan ges

--- # hello world.py
+++ # hello world.py
@@ -3,3 +3,7 @@ y <- "string"
 
 
 Now I want to see what happens if I do changes. This is a new branch. What happens here? 
+# what is going onhere? 
+
+
+I want to make some very important changes ot this file. 
