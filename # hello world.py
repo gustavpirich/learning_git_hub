54638ab6b86e90f8e683b971baffd5e82617d336
gustavpirich <gustav@pirich.at>
2024-10-31 14:54:57 +0100

    efwef

--- # hello world.py
+++ # hello world.py
@@ -7,4 +7,5 @@ y <- "string"
 Now I want to see what happens if I do changes. 
 
 
-# Now I would like to keep on working on this code file. 
+# Now I would like to keep on working on this code file. Ö
+Let me expand tha tcode fun. 
